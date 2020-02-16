6ef2a1edc2de1a2410997e25146f0a7671d0761d
mumuy <zhenghaole1990@126.com>
2020-02-16 23:42:18 +0800

    Update Browser.js

--- Browser.js
+++ Browser.js
@@ -115,7 +115,7 @@
                 is360 = true;
             }else if(chrome_vision>45){
                 is360 = _mime("type", "application/vnd.chromium.remoting-viewer");
-            }else if(chrome_vision>69){
+            }else if(chrome_vision>78){
                 is360 = _mime("type", "application/hwepass2001.installepass2001")||_mime("type", "application/asx");
             }
         }
