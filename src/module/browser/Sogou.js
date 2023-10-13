546d70dcff74c4e93ff52573b635c4afaf2513c7
zhenghaole <89932980@qq.com>
2023-10-13 10:18:43 +0800

    updated 搜狗浏览器处理

--- src/module/browser/Sogou.js
+++ src/module/browser/Sogou.js
@@ -4,7 +4,12 @@ export default {
         return ua.indexOf('MetaSr') > -1 || ua.indexOf('Sogou') > -1;
     },
     version(ua){
-        return ua.match(/^.*SE ([\d.X]+).*$/)?.[1]
+        var hash = {
+            '94':'12.0',
+            '80':'11.0'
+        };
+        var chrome_version = ua.match(/^.*Chrome\/([\d]+).*$/)?.[1]||'';
+        return hash[chrome_version]
         ||ua.match(/^.*SogouMobileBrowser\/([\d.]+).*$/)?.[1]
         ||'';
     }
