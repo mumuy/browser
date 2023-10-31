9412c169ec3e6e9291730e898ac282d7c3faa599
zhenghaole <89932980@qq.com>
2023-10-31 17:09:25 +0800

    updated 类型判断调整

--- src/module/gpu-loader.js
+++ src/module/gpu-loader.js
@@ -2,11 +2,11 @@ export default function(_,ua){
     _.gpu = '';
     _.gpuModel = '';
     if(self.document){
-        let $canvas = document.createElement('canvas'),
+        let $canvas = self.document.createElement('canvas'),
         webgl = $canvas.getContext('experimental-webgl'),
         debugInfo = webgl.getExtension('WEBGL_debug_renderer_info');
         let gpu_str = webgl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
         _.gpu = gpu_str.match(/ANGLE \((.+?),/)?.[1]||'';
-        _.gpuModel = gpu_str.match(/, (.+?) \(/)?.[1]||'';
+        _.gpuModel = gpu_str.match(/, (.+?) (\(|vs_)/)?.[1]||'';
     }
 };
