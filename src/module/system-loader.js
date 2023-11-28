fee66ce4a2cf30be86f86a3dfe1ccbeeb3474bd5
zhenghaole <89932980@qq.com>
2023-11-28 11:03:59 +0800

    updated 变量名调整

--- src/module/system-loader.js
+++ src/module/system-loader.js
@@ -1,6 +1,6 @@
 import _Windows from './system/Windows';
 import _Linux from './system/Linux';
-import _Mac_OS from './system/macOS';
+import _macOS from './system/macOS';
 import _Android from './system/Android';
 import _HarmonyOS from './system/HarmonyOS';
 import _Ubuntu from './system/Ubuntu';
@@ -17,7 +17,7 @@ import _WebOS from './system/WebOS';
 export default function(_,ua){
     _.system = '';
     _.systemVersion = '';
-    [_Windows, _Linux, _Mac_OS, _Android, _HarmonyOS, _Ubuntu, _FreeBSD, _Debian, _iOS, _Windows_Phone, _BlackBerry, _MeeGo, _Symbian, _Chrome_OS, _WebOS].forEach(function(item){
+    [_Windows, _Linux, _macOS, _Android, _HarmonyOS, _Ubuntu, _FreeBSD, _Debian, _iOS, _Windows_Phone, _BlackBerry, _MeeGo, _Symbian, _Chrome_OS, _WebOS].forEach(function(item){
         if(item.match(ua)){
             _.system = item.name;
             _.systemVersion = item.version(ua);
