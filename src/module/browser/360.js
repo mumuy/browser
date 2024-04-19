3c78cf4267ba9cd34b6f6caed9cdd26c9db9009d
zhenghaole <89932980@qq.com>
2024-04-19 15:15:09 +0800

    updated 浏览器判断调整

--- src/module/browser/360.js
+++ src/module/browser/360.js
@@ -2,25 +2,25 @@ import mime from '../method/mime.js';
 import _globalThis from '../runtime/globalThis.js';
 
 export default {
-    name:'360',
-    match(ua){
+    name: '360',
+    match(ua) {
         let isMatch = false;
-        if(_globalThis?.chrome){
+        if (_globalThis?.chrome) {
             let chrome_version = ua.replace(/^.*Chrome\/([\d]+).*$/, '$1');
-            if(mime("type", "application/360softmgrplugin")||mime("type", "application/mozilla-npqihooquicklogin")){
+            if (mime("type", "application/360softmgrplugin") || mime("type", "application/mozilla-npqihooquicklogin") || mime("type", "application/npjlgplayer3-chrome-jlp")) {
                 isMatch = true;
-            }else if(chrome_version>36&&_globalThis?.showModalDialog){
+            } else if (chrome_version > 36 && _globalThis?.showModalDialog) {
                 isMatch = true;
-            }else if(chrome_version>45){
+            } else if (chrome_version > 45) {
                 isMatch = mime("type", "application/vnd.chromium.remoting-viewer");
-                if(!isMatch&&chrome_version>=69){
-                    isMatch = mime("type", "application/hwepass2001.installepass2001")||mime("type", "application/asx");
+                if (!isMatch && chrome_version >= 69) {
+                    isMatch = mime("type", "application/hwepass2001.installepass2001") || mime("type", "application/asx");
                 }
             }
         }
-        return ua.indexOf('QihooBrowser') > -1||ua.indexOf('QHBrowser') > -1 || isMatch;
+        return ua.indexOf('QihooBrowser') > -1 || ua.indexOf('QHBrowser') > -1 || isMatch;
     },
-    version(ua){
-        return ua.match(/QihooBrowser(HD)?\/([\d.]+)/)?.[1]||'';
+    version(ua) {
+        return ua.match(/QihooBrowser(HD)?\/([\d.]+)/)?.[1] || '';
     }
 };
