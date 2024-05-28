ca08139e204ffebe2414d234b3b8a21f1386814d
zhenghaole <89932980@qq.com>
2024-05-28 17:02:26 +0800

    updated 兼容处理

--- src/module/gpu-loader.js
+++ src/module/gpu-loader.js
@@ -4,11 +4,13 @@ export default function(_){
     _.gpu = '';
     _.gpuModel = '';
     if(_globalThis?.document){
-        let $canvas = _globalThis.document.createElement('canvas'),
-        webgl = $canvas.getContext('experimental-webgl'),
-        debugInfo = webgl.getExtension('WEBGL_debug_renderer_info');
-        let gpu_str = webgl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
-        _.gpu = gpu_str.match(/ANGLE \((.+?),/)?.[1]||'';
-        _.gpuModel = gpu_str.match(/, (.+?) (\(|vs_)/)?.[1]||'';
+        let $canvas = _globalThis.document.createElement('canvas');
+        let webgl = $canvas.getContext('experimental-webgl');
+        if(webgl){
+            let debugInfo = webgl.getExtension('WEBGL_debug_renderer_info');
+            let gpu_str = webgl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+            _.gpu = gpu_str.match(/ANGLE \((.+?),/)?.[1]||'';
+            _.gpuModel = gpu_str.match(/, (.+?) (\(|vs_)/)?.[1]||'';
+        }
     }
 };
