776f5232deae89d9ce4c112ecb42156244035602
PASSER-BY <zhenghaole1990@126.com>
2024-06-13 23:17:56 +0800

    updated 添加IP和时区判断

---
+++ src/module/method/getPublicIP.js
@@ -0,0 +1,55 @@
+import globalObject from '../runtime/globalThis.js';
+
+const reg = {
+    'IPv4':/\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/,
+    'IPv6':/\b(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}\b/i
+};
+let RTCPeerConnection = globalObject.RTCPeerConnection || globalObject.mozRTCPeerConnection || globalObject.webkitRTCPeerConnection;
+const getPublicIP = () => {
+    const ipSet = new Set();
+    const onicecandidate = (ice) => {
+        const candidate = ice?.candidate?.candidate;
+        if (candidate) {
+            for (const regex of [reg['IPv4'], reg['IPv6']]) {
+                const match = candidate.match(regex);
+                if (match) {
+                    ipSet.add(match[0]);
+                }
+            }
+        }
+    };
+    return new Promise(function(resolve, reject){
+        const conn = new RTCPeerConnection({
+            iceServers: [{
+                urls: 'stun:stun.l.google.com:19302',
+            }]
+        });
+        conn.addEventListener('icecandidate', onicecandidate);
+        conn.createDataChannel('');
+        conn.createOffer().then((offer) => conn.setLocalDescription(offer), reject);
+        let count = 20;
+        let hander = null;
+        let closeConnect = function(){
+            try {
+                conn.removeEventListener('icecandidate', onicecandidate);
+                conn.close();
+            }catch{
+            }
+            hander&&clearInterval(hander);
+        };
+        hander = setInterval(function(){
+            let ips = [...ipSet];
+            if(ips.length){
+                closeConnect();
+                resolve(ips[0]);
+            }else if(count){
+                count--;
+            }else{
+                closeConnect();
+                resolve('');
+            }
+        }, 100);
+    });
+};
+
+export default getPublicIP;
--- src/module/other-loader.js
+++ src/module/other-loader.js
@@ -1,6 +1,10 @@
+import getPublicIP from './method/getPublicIP.js';
 
 export default function(_){
     // 类型判断
     _.isWebview = _.userAgent.indexOf('; wv)')>-1;
     _.isRobot = ['Googlebot', 'Baiduspider', 'Sogouspider', 'Bingbot', '360Spider', 'Bytespider', 'YandexBot'].includes(_.browser);
+    // IP地址
+    _.ip = getPublicIP();
+    _.timezone = Intl?.DateTimeFormat()?.resolvedOptions()?.timeZone;
 };
