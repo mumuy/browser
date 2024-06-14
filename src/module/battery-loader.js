5c681f8cc552f6aa979e9210d2f50fde22f02798
zhenghaole <89932980@qq.com>
2024-06-14 10:05:23 +0800

    updated 报错修复

--- src/module/battery-loader.js
+++ src/module/battery-loader.js
@@ -1,11 +1,11 @@
 import _globalThis from './runtime/globalThis.js';
 
 export default function(_){
-    if(navigator?.getBattery){
-        _.isCharging = navigator.getBattery().then((battery) => {
+    if(_globalThis.navigator?.getBattery){
+        _.isCharging = _globalThis.navigator.getBattery().then((battery) => {
             return battery.charging;
         });
-        _.battery = navigator.getBattery().then((battery) => {
+        _.battery = _globalThis.navigator.getBattery().then((battery) => {
             return +battery?.level||-1;
         });
     }else{
