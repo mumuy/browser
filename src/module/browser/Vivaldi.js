f6b64c782bbe9d95c962caeb0e2a57cbcefaa1c2
zhenghaole <zhenghaole1990@126.com>
2024-07-10 16:59:22 +0800

    updated 浏览器判断

--- src/module/browser/Vivaldi.js
+++ src/module/browser/Vivaldi.js
@@ -1,9 +1,15 @@
+import _globalThis from '../runtime/globalThis.js';
+
 export default {
     name:'Vivaldi',
     match(ua){
-        return ua.includes('Vivaldi');
+        let isMatch = false;
+        if(_globalThis?.navigator?.userAgentData?.brands.filter(item=>item.brand=='Not:A-Brand').length){
+            isMatch = true;
+        }
+        return ua.includes('Vivaldi')||isMatch;
     },
     version(ua){
-        return ua.match(/Vivaldi\/([\d.]+)/)?.[1]||'';
+        return ua.match(/Vivaldi\/([\d.]+)/)?.[1]||ua.match(/Chrome\/([\d.]+)/)?.[1]||'';
     }
 };
