fb1bccdd9aea9feda6c30f620624eacf04b38dd0
路人甲 <zhenghaole1990@126.com>
2024-07-11 22:42:22 +0800

    updated 测试调整

--- src/module/battery-loader.js
+++ src/module/battery-loader.js
@@ -3,7 +3,7 @@ import _globalThis from './runtime/globalThis.js';
 export default function(_){
     if(_globalThis?.navigator?.getBattery){
         _.isCharging = _globalThis.navigator.getBattery().then((battery) => {
-            return battery.charging;
+            return battery?.charging;
         });
         _.battery = _globalThis.navigator.getBattery().then((battery) => {
             return +battery?.level||-1;
