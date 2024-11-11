62dfa160773d3c0e4a9d0de359da16688be43419
zhenghaole <89932980@qq.com>
2024-11-11 09:51:57 +0800

    updated json文件加载方法调整

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';          // 使用node_modules包
 import terser from '@rollup/plugin-terser';                 // 代码压缩
 import babel from '@rollup/plugin-babel';                   // ECMAScript兼容
-import pkg from './package.json' assert { type:'json' };    // 获取package信息
+import pkg from './package.json' with { type:'json' };      // 获取package信息
 
 // 版权信息
 const repository = pkg.repository.url.replace(/(.+)(:\/\/.+)\.git$/,'https$2');
