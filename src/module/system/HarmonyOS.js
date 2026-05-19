84434dcb1b651815f7ebe3224553b70982ce6c2a
mumuy <zhenghaole1990@126.com>
2026-05-19 18:35:42 +0800

    updated 鸿蒙判断

--- src/module/system/HarmonyOS.js
+++ src/module/system/HarmonyOS.js
@@ -3,7 +3,7 @@ import userAgent from '../runtime/userAgent.js';
 export default {
     name:'HarmonyOS',
     parse(ua = userAgent){
-        let version = ua.match(/HarmonyOS (\d+.\d+)/)?.[1]||ua.match(/OpenHarmony(\d+.\d+)/)?.[1]||'';
+        let version = ua.match(/HarmonyOS (\d+.\d+)/)?.[1]||ua.match(/OpenHarmony(\d+.\d+)/)?.[1]||ua.match(/ArkWeb\/(\d+.\d+)/)?.[1]||'';
         if(!version){
             let v = +ua.match(/HMSCore (\d+.\d+)/)?.[1]||0;
             if(v>=6.1){
@@ -15,12 +15,14 @@ export default {
             }else if(v>=4.0){
                 version =  '1.0';
             }
-            let av = ua.match(/Android (\d+);/)?.[1]||0;
+        }
+        if(!version){
+            let v = ua.match(/Android (\d+);/)?.[1]||0;
             let androidMap = {
                 '10':'3.0',
                 '12':'4.0'
             };
-            version =  androidMap[av]||version;
+            version =  androidMap[v]||version;
         }
         return {
             is:ua.includes('HarmonyOS')||ua.includes('OpenHarmony')||ua.includes('ArkWeb'),
